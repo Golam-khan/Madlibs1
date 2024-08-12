2e047fb186ae5453c542dd4378ec08299bcfbcdc
Golam Khan <ishratlubna2@gmail.com>
2024-08-12 21:43:05 +0000

    1st commit

---
+++ Madlibs.js
@@ -0,0 +1,19 @@
+//variables--anolgy--moving box - labaled on the outside,stores stuff inside
+
+// let keyword declares a variables that can have  its value changed
+//const --value can not change
+//data types for word =string
+let noun1 = "dog"
+
+let adjective="quick"
+
+
+prompt=("enter a verb")
+
+//add our variables into 1 string
+let output = "The"+ noun1 + "jumped over the" + adjective
+
+//add variable that 
+
+console.log(output)
+console.log(7+2)
